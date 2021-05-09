a729e71efb7e1eec0d6d67eea7ce9f50dc7899f2
Adrian Jesus Rodriguez Brito <adri.rodri.brito@gmail.com>
2021-05-09 21:10:07 +0100

    edit and done Todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -10,16 +10,24 @@ export class TodosComponent implements OnInit {
 
   todos:Todo[];
   inputTodo:string = "";
+  todosDone:Todo[];
+  selectedTodo: Todo;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.selectedTodo = new Todo();
     this.todos = []
+    this.todosDone = []
   }
 
   toggleDone(id:number) {
     this.todos.map((v,i) => {
-      if(i == id) v.completed = !v.completed;
+      if(i == id) {
+        v.completed = !v.completed;
+        if(!this.todosDone.includes(v)) this.todosDone.push(v);
+      }
+
       return v;
     })
   }
@@ -33,10 +41,37 @@ export class TodosComponent implements OnInit {
       this.todos.push({
         content: this.inputTodo,
         completed: false,
+        date: new Date()
       });
     }
 
     this.inputTodo = "";
   }
 
+  editTodo(todo: Todo){
+    const div = document.getElementById("edit");
+    this.selectedTodo = todo;
+    div.classList.toggle("notShow");
+  }
+  
+  orderDate(){
+    this.todos.sort( (a,b) => {
+      return  a.date.getTime() - b.date.getTime();
+    })
+  }
+
+  orderAlpha(){
+    this.todos.sort( (a,b) => {
+      let aContent = a.content.toLowerCase();
+      let bContent = b.content.toLowerCase();
+
+      return aContent < bContent ? -1 : aContent > bContent ? 1 : 0;
+    })
+  }
+
+  orderState(){
+    this.todos.sort( a => {
+      return a.completed ? 1 : -1;
+    });
+  }
 }
